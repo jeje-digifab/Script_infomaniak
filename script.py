31154a4a35e595404d48cea76fb4615064034047
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 -- NOK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 <j.romand.digifab@gmail.com>
2025-08-09 13:15:08 +0200

    Refactor txt_record function to improve readability and error handling

--- script.py
+++ script.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
 
 import dns.resolver
-#import dns.name
 import re
 
 url = 'candiharvest.infomanihack.ch'
 
 
-# curl --header "X-Candidate-Id:'id={id} https://candiharvest.infomanihack.ch/
+def txt_record():
+    """
+    Function to fetch TXT records for a given URL.
+    """
+    try:
+        result = dns.resolver.resolve(url, 'TXT')
+
+        for val in result:
+            id = val.to_text()
+            id = re.sub(r'"', '', id)
+            print(id)
+    except Exception as e:
+        print(f"Error fetching TXT record: {e}")
+        return None
 
 
-# Finding TXT record
-result = dns.resolver.resolve(url, 'TXT')
-
-# Printing record
-for val in result:
-    id = val.to_text()
-    id = re.sub(r'"', '', id)
-    print(id)
+if __name__ == "__main__":
+    # main()
+    txt_record()
